d19f9f2e4dda19e5731219270e84308c89e0bc60
unblee <unblee@users.noreply.github.com>
2021-12-02 23:48:59 +0900

    dprint fmt

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import React from 'react'
-import { Stage,Graphics } from '@inlet/react-pixi';
+import { Graphics, Stage } from "@inlet/react-pixi";
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
     const draw = React.useCallback(g => {
-    g.clear()
-    g.beginFill(0xff3300)
-    g.lineStyle(4, 0xffd900, 1)
-    g.moveTo(50, 50)
-    g.lineTo(250, 50)
-    g.lineTo(100, 100)
-    g.lineTo(50, 50)
-    g.endFill()
-    g.lineStyle(2, 0x0000ff, 1)
-    g.beginFill(0xff700b, 1)
-    g.drawRect(50, 150, 120, 120)
-    g.lineStyle(2, 0xff00ff, 1)
-    g.beginFill(0xff00bb, 0.25)
-    g.drawRoundedRect(150, 100, 300, 100, 15)
-    g.endFill()
-    g.lineStyle(0)
-    g.beginFill(0xffff0b, 0.5)
-    g.drawCircle(470, 90, 60)
-    g.endFill()
-  }, [])
-  return (
-    <Stage width={600} height={300} options={{ backgroundColor: 0xffffff }}>
-      <Graphics draw={draw} />
-    </Stage>
-  )
+        g.clear();
+        g.beginFill(0xff3300);
+        g.lineStyle(4, 0xffd900, 1);
+        g.moveTo(50, 50);
+        g.lineTo(250, 50);
+        g.lineTo(100, 100);
+        g.lineTo(50, 50);
+        g.endFill();
+        g.lineStyle(2, 0x0000ff, 1);
+        g.beginFill(0xff700b, 1);
+        g.drawRect(50, 150, 120, 120);
+        g.lineStyle(2, 0xff00ff, 1);
+        g.beginFill(0xff00bb, 0.25);
+        g.drawRoundedRect(150, 100, 300, 100, 15);
+        g.endFill();
+        g.lineStyle(0);
+        g.beginFill(0xffff0b, 0.5);
+        g.drawCircle(470, 90, 60);
+        g.endFill();
+    }, []);
+    return (
+        <Stage width={600} height={300} options={{ backgroundColor: 0xffffff }}>
+            <Graphics draw={draw} />
+        </Stage>
+    );
 }
 
-export default App
+export default App;
