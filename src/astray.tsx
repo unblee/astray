46bb8a0cfbf501157d8f3d24acc43d3dca0464ff
unblee <unblee@users.noreply.github.com>
2022-02-11 01:23:30 +0900

    Get URL of a chrome current window

--- src/astray.tsx
+++ src/astray.tsx
@@ -2,6 +2,6 @@ import { showHUD } from "@raycast/api";
 import { runAppleScript } from "run-applescript";
 
 export default async function main() {
-  const result = await runAppleScript('return "unicorn"');
+  const result = await runAppleScript('tell application "Google Chrome" to get URL of active tab of first window');
   await showHUD(result);
 }
